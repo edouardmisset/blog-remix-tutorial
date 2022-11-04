9fbb8f830549d040a1fee6ec58532e74b6a83160
Edouard Misset <edouard.misset@intest.info>
2022-11-04 17:45:31 +0100

    lOADING

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,4 +1,4 @@
-import { Form, useActionData } from "@remix-run/react";
+import { Form, useActionData, useTransition } from "@remix-run/react";
 
 import type { ActionFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
@@ -16,6 +16,9 @@ type ActionData =
   | undefined;
 
 export const action: ActionFunction = async ({ request }) => {
+  // TODO: remove me
+  await new Promise((res) => setTimeout(res, 1000));
+
   const formData = await request.formData();
 
   const title = formData.get("title");
@@ -45,6 +48,9 @@ const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`
 
 export default function NewPost() {
   const errors = useActionData();
+
+  const transition = useTransition();
+  const isCreating = Boolean(transition.submission);
 
   return (
     <Form method="post">
@@ -86,8 +92,9 @@ export default function NewPost() {
         <button
           type="submit"
           className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+          disabled={isCreating}
         >
-          Create Post
+          {isCreating ? "Creating..." : "Create Post"}
         </button>
       </p>
     </Form>
