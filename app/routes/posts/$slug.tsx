23de20cef723ad1233a88ac04d6a0ea91cc43688
Edouard Misset <edouard.misset@intest.info>
2022-11-04 17:02:03 +0100

    Added the slug from the server

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,17 +1,27 @@
-import { json } from "@remix-run/node";
 import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
 
-export const loader: LoaderFunction = async ({ params }) =>
-  json({ slug: params.slug });
+import type { Post } from "~/models/post.server";
+import { getPost } from "~/models/post.server";
+
+type LoaderData = { post: Post };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.slug, `params.slug is required`);
+
+  const post = await getPost(params.slug);
+  invariant(post, `Post not found: ${params.slug}`);
+
+  return json<LoaderData>({ post });
+};
 
 export default function PostSlug() {
-  const { slug } = useLoaderData();
+  const { post } = useLoaderData<LoaderData>();
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">
-        Some Post: {slug}
-      </h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
     </main>
   );
 }
